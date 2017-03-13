e0b80b6552820aff2030332e568c72f386f808de
Jabp95 <ramik199517@gmail.com>
2017-03-13 17:50:22 +0100

    Forms de nuevos usuarios creados

--- iFarma/public_html/js/main.js
+++ iFarma/public_html/js/main.js
@@ -3,12 +3,34 @@
  */
 //Funcion principal
 $('document').ready(function(){
+    
     console.log("DOM inicializado");
+    $("#pagoEfectivo").hide();
+    $("#pagoTarjeta").hide();
+    $("#pagoPayPal").hide();
+    
     $('#datePicker').datepicker({
         format: 'dd/mm/yyyy'
     }).on('changeDate', function(e) {
         // Revalidate the date field
         $('#eventForm').formValidation('revalidateField', 'date');
+    });
+    $('input[type=radio][name=formaPago]').change(function() {
+        if (this.value == 'p') {
+            $("#pagoEfectivo").hide();
+            $("#pagoTarjeta").hide();
+            $("#pagoPayPal").show();
+        }
+        else if (this.value == 't') {
+            $("#pagoEfectivo").hide();
+            $("#pagoTarjeta").show();
+            $("#pagoPayPal").hide();
+        }
+        else if (this.value == 'e') {
+            $("#pagoEfectivo").show();
+            $("#pagoTarjeta").hide();
+            $("#pagoPayPal").hide();
+        }
     });
 });
 
